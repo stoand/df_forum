651bdbd769d9b3a4a10fb358796dd5d913598063
Andreas Stocker <shadowstep7@gmail.com>
2022-08-23 16:45:58 +0200

    forum: initial home page loading event

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,57 @@ extern crate differential_dataflow;
 extern crate timely;
 
 use differential_dataflow::input::InputSession;
-use differential_dataflow::operators::Join;
 
-#[derive(Clone,Debug,PartialEq, Eq, PartialOrd, Ord)]
-enum EventType {
-    Stuff0,
-    Stuff1,
-}
+// #[derive(Clone,Debug,PartialEq, Eq, PartialOrd, Ord)]
+// enum EventType {
+//     Stuff0,
+//     Stuff1,
+// }
 
 fn main() {
     run0();
 }
 
+fn add_rm_str(val: &isize) -> String {
+    if *val == 1 {
+        "add".into()
+    } else {
+        "rm".into()
+    }
+}
+
 fn run0() {
-    
     timely::execute_from_args(std::env::args(), move |worker| {
         let mut input = InputSession::new();
 
         worker.dataflow(|scope| {
             let manages = input.to_collection(scope);
 
-            manages
-                // .map(|(m2, m1)| (m1, m2))
-                // .join(&manages)
-                .inspect(|x| println!("{:?}", x));
-        });
+            // manages
+            //     // .map(|(m2, m1)| (m1, m2))
+            //     // .join(&manages)
+            //     .inspect(|x| println!("  -- {:?}", x));
 
-        let size = std::env::args().nth(1).unwrap().parse().unwrap();
+            manages
+                .filter(|&tup| match tup {
+                    ("session", "page", ("home", _attach_to)) => true,
+                    _ => false,
+                })
+                .inspect(|((_el, _at, (_val0, attach_to)), time, add_rm)| {
+                    println!(
+                        "{} home page to element with id: {:?}; at time {:?}",
+                        add_rm_str(add_rm),
+                        attach_to,
+                        time
+                    )
+                });
+        });
         input.advance_to(0);
-        for person in 0..size {
-            
-            input.insert((person / 2, EventType::Stuff0));
-        }
+
+        // index load page event fires on first load
+        input.insert(("session", "page", ("home", "02312")));
 
         input.advance_to(1);
-        input.remove((2, EventType::Stuff0));
     })
     .expect("fail");
 }
