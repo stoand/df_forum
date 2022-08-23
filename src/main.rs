36ec40a7883c2450bc43f44e050c6004cf7af88e
Andreas Stocker <shadowstep7@gmail.com>
2022-08-23 15:05:18 +0200

    forum: setup timely

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+extern crate timely;
+
 fn main() {
     println!("Hello, world!");
 }
 
 #[cfg(test)]
 mod tests {
+    use timely::dataflow::operators::{Inspect, ToStream};
 
     #[test]
     fn it_works() {
         assert_eq!(1, 1, "all");
+
+        timely::example(|scope| {
+            (0..10)
+                .to_stream(scope)
+                .inspect(|x| println!("seen {:?}", x));
+        });
     }
 }
