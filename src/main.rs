73b1ef98b119d62179e92ae9c2426b42eb2106a6
Andreas Stocker <shadowstep7@gmail.com>
2022-08-23 16:16:49 +0200

    forum: serialize custom enum

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,18 @@ extern crate timely;
 use differential_dataflow::input::InputSession;
 use differential_dataflow::operators::Join;
 
+#[derive(Clone,Debug,PartialEq, Eq, PartialOrd, Ord)]
+enum EventType {
+    Stuff0,
+    Stuff1,
+}
+
 fn main() {
     run0();
 }
 
 fn run0() {
+    
     timely::execute_from_args(std::env::args(), move |worker| {
         let mut input = InputSession::new();
 
@@ -16,16 +23,20 @@ fn run0() {
             let manages = input.to_collection(scope);
 
             manages
-                .map(|(m2, m1)| (m1, m2))
-                .join(&manages)
+                // .map(|(m2, m1)| (m1, m2))
+                // .join(&manages)
                 .inspect(|x| println!("{:?}", x));
         });
 
         let size = std::env::args().nth(1).unwrap().parse().unwrap();
         input.advance_to(0);
         for person in 0..size {
-            input.insert((person / 2, person));
+            
+            input.insert((person / 2, EventType::Stuff0));
         }
+
+        input.advance_to(1);
+        input.remove((2, EventType::Stuff0));
     })
     .expect("fail");
 }
