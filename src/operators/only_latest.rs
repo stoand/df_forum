d850c7658822a5bd7125ca874216ba86e445d996
Andreas Stocker <shadowstep7@gmail.com>
2022-09-14 20:38:09 +0200

    only_latest - can use generic Abelians instead of hardcoded isize

--- src/operators/only_latest.rs
+++ src/operators/only_latest.rs
@@ -1,29 +1,32 @@
-use differential_dataflow::difference::{Abelian, Semigroup};
+use differential_dataflow::difference::{Abelian, Monoid};
 use differential_dataflow::hashable::Hashable;
 use differential_dataflow::lattice::Lattice;
 use differential_dataflow::operators::Reduce;
 use differential_dataflow::AsCollection;
-use differential_dataflow::{Collection, Data, ExchangeData};
+use differential_dataflow::{Collection, ExchangeData};
 use timely::dataflow::operators::Map;
 use timely::dataflow::*;
 
-// TODO: replace the hardcoded isize with a generic Semigroup
-
-pub trait OnlyLatest<G: Scope, K: Data, V: Data>
-where
-    G::Timestamp: Lattice + Ord,
-{
-    fn only_latest(&self) -> Collection<G, (K, V), isize>;
-}
-
-impl<G, K, V> OnlyLatest<G, K, V> for Collection<G, (K, V), isize>
+pub trait OnlyLatest<G, K, V, R>
 where
     G: Scope,
     G::Timestamp: Lattice + Ord,
     K: ExchangeData + Hashable,
     V: ExchangeData,
+    R: Abelian + ExchangeData,
 {
-    fn only_latest(&self) -> Collection<G, (K, V), isize> {
+    fn only_latest(&self) -> Collection<G, (K, V), R>;
+}
+
+impl<G, K, V, R> OnlyLatest<G, K, V, R> for Collection<G, (K, V), R>
+where
+    G: Scope,
+    G::Timestamp: Lattice + Ord,
+    K: ExchangeData + Hashable,
+    V: ExchangeData,
+    R: Abelian + ExchangeData,
+{
+    fn only_latest(&self) -> Collection<G, (K, V), R> {
         self.inner
             .map(|((id, persisted), time, diff)| {
                 // let reduce sort by time
@@ -31,20 +34,22 @@ where
             })
             .as_collection()
             .reduce(|_key, inputs, outputs| {
-                // log(&format!(
-                //     "key = {:?}, input = {:?}, output = {:?}",
-                //     _key, inputs, outputs
-                // ));
+                let abelian = inputs[0].clone().1;
+                let positive_abelian: R = if abelian > Monoid::zero() {
+                    abelian
+                } else {
+                    abelian.neg()
+                };
+                let negative_abelian = positive_abelian.clone().neg();
 
                 for i in 0..inputs.len() {
-                    
                     let value_ref: &(_, V) = inputs[i].clone().0;
                     let value: (_, V) = value_ref.clone();
-                    
                     if i == inputs.len() - 1 {
-                        outputs.push((value, 1));
+                        // TODO: replace zeros with 1 and -1
+                        outputs.push((value, positive_abelian.clone()));
                     } else {
-                        outputs.push((value, -1));
+                        outputs.push((value, negative_abelian.clone()));
                     }
                 }
             })
