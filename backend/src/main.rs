1e3eddbd2e2cdc2385f2a7871b9dcf288ab586cf
Andreas Stocker <shadowstep7@gmail.com>
2022-09-16 11:39:57 +0200

    add dep tokio_tungstenite

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,27 @@
 extern crate df_forum_backend;
+extern crate tokio;
+extern crate tokio_tungstenite;
+extern crate futures_channel;
+extern crate futures_util;
+
+use std::{
+    collections::HashMap,
+    env,
+    io::Error as IoError,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
+
+use futures_channel::mpsc::{unbounded, UnboundedSender};
+use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
+
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::tungstenite::protocol::Message;
+
+type Tx = UnboundedSender<Message>;
+type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
+
+
 
 fn main() {
     println!("Hello, world!");
