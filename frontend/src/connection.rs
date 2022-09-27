b284357be4feff63866f2626b88d13a54a57b80b
Andreas Stocker <shadowstep7@gmail.com>
2022-09-27 22:49:10 +0200

    frontend: fix warnings

--- frontend/src/connection.rs
+++ frontend/src/connection.rs
@@ -1,6 +1,6 @@
 use web_sys::Event;
 use web_sys::MessageEvent as WebSocketMessageEvent;
-use web_sys::{Document, Element, HtmlElement, HtmlInputElement, Storage, WebSocket};
+use web_sys::WebSocket;
 
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -13,7 +13,6 @@ use crate::persisted::Persisted;
 use crate::query_result::QueryResult;
 
 pub struct FrontendConnection {
-    buffer: Rc<RefCell<Vec<QueryResult>>>,
     websocket: Rc<RefCell<WebSocket>>,
     pub onmessage: Option<fn(Vec<QueryResult>) -> ()>,
 }
@@ -31,7 +30,6 @@ impl FrontendConnection {
             .borrow()
             .set_onopen(Some(onopen.as_ref().unchecked_ref()));
         onopen.forget();
-        
         let onclose = Closure::<dyn FnMut(Event)>::new(move |_event: Event| {
             log(&format!("websocket close"));
         });
@@ -41,24 +39,19 @@ impl FrontendConnection {
             .set_onclose(Some(onclose.as_ref().unchecked_ref()));
         onclose.forget();
 
-        let buffer = Rc::new(RefCell::new(Vec::new()));
-        let buffer0 = buffer.clone();
-
         FrontendConnection {
-            buffer: buffer0,
             websocket: websocket0.clone(),
             onmessage: None,
         }
     }
 
     pub fn init_on_parsed_message(&self, on_parsed_message: Box<dyn Fn(Vec<QueryResult>)>) {
-
         let onmessage = Closure::<dyn FnMut(WebSocketMessageEvent)>::new(
             move |message: WebSocketMessageEvent| {
                 let data = message.data().as_string().unwrap();
                 log(&format!("got websocket message: {:?}", data));
 
-                let mut parsed_data: Vec<QueryResult> =
+                let parsed_data: Vec<QueryResult> =
                     serde_json::from_str(&data).expect("could not parse QueryResults");
 
                 on_parsed_message(parsed_data);
@@ -70,7 +63,6 @@ impl FrontendConnection {
         onmessage.forget();
     }
 
-        
     pub fn send_transaction(&self, persisted_items: Vec<Persisted>) {
         let msg = serde_json::to_string(&persisted_items).unwrap();
 
